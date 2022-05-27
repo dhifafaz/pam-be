433d4881ce04166c7a504b4b2082f394167566e8
dhifafaz <dhifaf.az@gmail.com>
2022-05-27 07:32:53 +0700

    be hosting bismillah lancar

--- movix_api/admin.py
+++ movix_api/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Film
+from django.utils.html import format_html
 
 class FilmData(admin.ModelAdmin):
     list_per_page = 10
     list_display = ('title', 'rating', 'tahun_terbit', 
                 'genre', 'durasi', 'description', 
                 'sutradara', 'image')
+    readonly_fields = ('image', )
+    
+    def image(self, obj):
+        return format_html("<a href='{url}'>{url}</a>", url=obj.image)
 
 admin.site.register(Film, FilmData)
--- movix_api/models.py
+++ movix_api/models.py
@@ -10,8 +10,8 @@ class Film(models.Model):
     durasi = models.CharField(max_length=10)
     description = models.TextField(max_length=1000)
     sutradara = models.CharField(max_length=150)
-    image = models.ImageField(upload_to='poster_film/')
-    
+    # image = models.ImageField(upload_to='poster_film/')
+    image = models.URLField(max_length=300, blank=True)
     def __str__(self):
         return  self.title
     
