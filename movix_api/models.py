368352d6febb28e000512922055c8b0075590a08
dhifafaz <dhifaf.az@gmail.com>
2022-05-27 08:24:02 +0700

    be hosting bismillah lancar

--- movix_api/models.py
+++ movix_api/models.py
@@ -11,7 +11,7 @@ class Film(models.Model):
     description = models.TextField(max_length=1000)
     sutradara = models.CharField(max_length=150)
     # image = models.ImageField(upload_to='poster_film/')
-    image = models.URLField(max_length=300, blank=True)
+    image = models.CharField(max_length=300, blank=True)
     
     def __str__(self):
         return  self.title
