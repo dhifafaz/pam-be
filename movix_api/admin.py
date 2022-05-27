5643d05b5e1bd75777a744f54f0de4400894852d
dhifafaz <dhifaf.az@gmail.com>
2022-05-27 07:41:27 +0700

    be hosting bismillah lancar

--- movix_api/admin.py
+++ movix_api/admin.py
@@ -4,12 +4,21 @@ from django.contrib import admin
 from .models import Film
 from django.utils.html import format_html
 
+@admin.display(description="image")
 class FilmData(admin.ModelAdmin):
     list_per_page = 10
     list_display = ('title', 'rating', 'tahun_terbit', 
                 'genre', 'durasi', 'description', 
-                'sutradara', 'image')
-    readonly_fields = ('image_file', )
+                'sutradara', 'image_file')
+    list_filter = ('title','rating', 'genre', 'tahun_terbit')
+    # fieldsets = ('title', 'rating', 'tahun_terbit', 
+    #             'genre', 'durasi', 'description', 
+    #             'sutradara', 'image_file')
+    # add_fieldsets = ('title', 'rating', 'tahun_terbit', 
+    #             'genre', 'durasi', 'description', 
+    #             'sutradara', 'image_file')
+    search_fields = ('title', 'sutradara')
+    # readonly_fields = ('image_file', )
     
     def image_file(self, obj):
         return format_html("<a href='{url}'>{url}</a>", url=obj.image)
